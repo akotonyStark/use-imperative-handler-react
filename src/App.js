bbeea606048dbc5a15b088f134e7faa0143b1ed1
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-11-24 07:47:02 +0000

    updated with AppContext

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg'
 import './App.css'
 import ParentComponent from './components/ParentComponent'
+import { createContext } from 'react'
+
+export const AppContext = createContext(null)
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <ParentComponent />
+
+        <AppContext.Provider
+          value={{ author: 'Augustine', framework: 'ReactJS' }}
+        >
+          <ParentComponent />
+        </AppContext.Provider>
       </header>
     </div>
   )
